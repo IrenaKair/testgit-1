b95b8be324c5ecf8205fd214ddaaa597c67dee4e
Robertk13 <kaczmarekrob13@gmail.com>
2016-09-30 19:48:58 +0200

    now is good file

---
+++ Igor.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Robert on 2016-09-30.
+ */
+public class Igor {
+    public static void main ( String []args){
+        System.out.println(" Cześć Igor - jak to dostałeś to jest OK. Pozdrawiam");
+    }
+}
