046618c162fb75d93b5cf8a557563095a2f64512
IrenaKair <kairena68@gmail.com>
2016-10-06 17:25:51 +0200

    Add file
    Dodanie pliku- różne typy zmiennych - Irena

---
+++ zadanie1Ira.java
@@ -0,0 +1,41 @@
+/**
+ * Created by Irena on 2016-10-06.
+ */
+public class zadanie1Ira {
+    public static void main(String[] args){
+        int a = 24;
+        System.out.println("a = "+ a);
+        byte b = 9;
+        System.out.println(" b = "+ b);
+        short s = 2;
+        System.out.println("s = " +s);
+        long l = 500l;
+        System.out.println("l = "+ l);
+        float f = 3000f;
+        System.out.println("f = " +f);
+        double d = 86.32;
+        System.out.println("d = "+ d);
+        boolean bo = true;
+        System.out.println("boolean = "+ b);
+        char ch = 'A';
+        int j = a;
+        j--;
+        System.out.println("zmniejszamy a o jeden i teraz równa się: " + j);
+        int resultAB = a+b;
+        System.out.println("a+b = " + resultAB);
+        int resultAb = a-b;
+        double resultLD = l/d;
+        System.out.println("l/d = " + resultLD);
+        int resultBS = b * s;
+        System.out.println("b*s = " + resultBS);
+        float resultMod = a%b;
+        System.out.println("reszta z dzielenia a i b = " + resultMod);
+        String aaa = "To";
+        String bbb = "jest";
+        String ccc = "test";
+        System.out.println(aaa +" "+ bbb+" "+ ccc );
+        boolean g = a < b;
+        System.out.println(" a< b = "+ g);
+
+    }
+}
